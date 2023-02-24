02f49de02ef2fc62ac33eb489fda967a05b1f6b3
HosseinDvz <62449282+HosseinDvz@users.noreply.github.com>
2023-02-23 23:47:10 -0500

    Add files via upload

---
+++ CNNmodel.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 19:29:05 2023
+
+@author: hosdv
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+import os
+
+import numpy as np
+
+#import pandas as pd
+import matplotlib.pyplot as plt
+
+# ignore harmless warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+train_transform = transforms.Compose([
+        transforms.RandomRotation(10),      # rotate +/- 10 degrees
+        transforms.RandomHorizontalFlip(),  # reverse 50% of images
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+test_transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+
+root = 'D:/Fulhaus/Data for test'
+
+train_data = datasets.ImageFolder(os.path.join(root, 'train'), transform=train_transform)
+test_data = datasets.ImageFolder(os.path.join(root, 'test'), transform=test_transform)
+
+torch.manual_seed(42)
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=True)
+
+class_names = train_data.classes
+
+print(class_names)
+print(f'Training images available: {len(train_data)}')
+print(f'Testing images available:  {len(test_data)}')
+
+
+for images, labels in train_loader:
+    break
+
+images.shape
+
+
+
+# Print the labels
+#print('Label:', labels.numpy())
+#print('Class:', *np.array([class_names[i] for i in labels]))
+
+im = make_grid(images, nrow=5)  # the default nrow is 8
+
+# Inverse normalize the images
+inv_normalize = transforms.Normalize(
+    mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
+    std=[1/0.229, 1/0.224, 1/0.225]
+)
+im_inv = inv_normalize(im)
+
+# Print the images
+plt.figure(figsize=(12,4))
+plt.imshow(np.transpose(im_inv.numpy(), (1, 2, 0)));
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(54*54*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 54*54*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
+
+
+torch.manual_seed(101)
+CNNmodel = ConvolutionalNetwork()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(CNNmodel.parameters(), lr=0.001)
+CNNmodel
+
+
+def count_parameters(model):
+    params = [p.numel() for p in model.parameters() if p.requires_grad]
+    for item in params:
+        print(f'{item:>8}')
+    print(f'________\n{sum(params):>8}')
+
+count_parameters(CNNmodel)
+
+
+import time
+start_time = time.time()
+
+epochs = 1
+
+
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    
+    i += 1
+    trn_corr = 0
+    tst_corr = 0
+    
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        # Limit the number of batches
+        b+=1
+        
+        # Apply the model
+        y_pred = CNNmodel(X_train)
+        loss = criterion(y_pred, y_train)
+ 
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+        
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Print interim results
+        if b%2 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10*b:6}/240]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item()*100/(10*b):7.3f}%')
+
+    train_losses.append(loss.detach().numpy())
+    train_correct.append(trn_corr.detach().numpy())
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+            # Limit the number of batches
+
+            # Apply the model
+            y_val = CNNmodel(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1] 
+            tst_corr += (predicted == y_test).sum()
+
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss.detach().numpy())
+    test_correct.append(tst_corr.detach().numpy())
+
+print(f'\nDuration: {time.time() - start_time:.0f} seconds') # print the time elapsed
+
+
+torch.save(CNNmodel.state_dict(), 'CustomImageCNNModel.pth')
+
+plt.plot(train_losses, label='training loss')
+plt.plot(test_losses, label='validation loss')
+plt.title('Loss at the end of each epoch')
+plt.legend();
+
+
+
+
+
+
